23c2b09e7f84f7f8104f92b3ceef688749abdbd6
Victor Manoel <victolopes2002@gmail.com>
2024-09-03 08:15:19 -0300

    add pergunta ao user o nome dele

--- Renomear Arquivos/renomeador-de-arquivos.py
+++ Renomear Arquivos/renomeador-de-arquivos.py
@@ -1,6 +1,8 @@
 import os
 from tkinter.filedialog import askdirectory
 
+nome_User = str(input("Qual o seu nome: "))
+
 caminho = askdirectory(title="Selecione uma pasta")
 os.chdir(caminho)
 
@@ -8,5 +10,5 @@ lista_de_arquivos = os.listdir(caminho)
 
 for arquivo in lista_de_arquivos:
     if os.path.isfile(arquivo):
-        novo_nome = f'Arquivos do Victor - {arquivo}'
+        novo_nome = f'Arquivos do(a) {nome_User} - {arquivo}'
         os.rename(arquivo, novo_nome)
