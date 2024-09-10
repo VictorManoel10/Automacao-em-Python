a3f89b75f429b9685c956fd3b5c50856fd07a733
Victor Manoel <victolopes2002@gmail.com>
2024-09-10 09:27:14 -0300

    Add interface(janela) ao app de organizar arquivos

--- Orgazinador de Arquivos em pastas/organizador-de-arquivos.py
+++ Orgazinador de Arquivos em pastas/organizador-de-arquivos.py
@@ -1,21 +1,46 @@
 import os
 from tkinter.filedialog import askdirectory
+from tkinter import *
 
-caminho = askdirectory(title="Selecione uma pasta")
+def orgArq():
+    caminho = askdirectory(title="Selecione uma pasta")
 
-lista_arquivos = os.listdir(caminho)
+    if not caminho:  # Caso o usuário cancele a seleção da pasta
+        return
 
-locais = {
-    "imagens": [".png", ".jpg"],
-    "planilhas": [".xlsx"],
-    "pdfs": [".pdf"],
-    "csv": [".csv"],
-}
+    lista_arquivos = os.listdir(caminho)
 
-for arquivo in lista_arquivos:
-    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
-    for pasta in locais:
-        if extensao in locais[pasta]:
-            if not os.path.exists(f"{caminho}/{pasta}"):
-                os.mkdir(f"{caminho}/{pasta}")
-            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
+    locais = {
+        "Imagens e Videos": [".png", ".jpg", ".mp4", ".avi", ".wmv"],
+        "Musicas": [".mp3", ".wav"],
+        "planilhas": [".xlsx"],
+        "Pdfs": [".pdf"],
+        "Winrar": [".rar"],
+        "Arquivos de Texto": [".docx", ".txt",]
+    }
+
+    for arquivo in lista_arquivos:
+        nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+        for pasta in locais:
+            if extensao in locais[pasta]:
+                if not os.path.exists(f"{caminho}/{pasta}"):
+                    os.mkdir(f"{caminho}/{pasta}")
+                os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
+
+    txt_end.grid(column=0, row=2, padx=10, pady=10)
+
+
+janela = Tk()
+janela.title("Organizador de arquivos")
+
+txt_main = Label(janela, text="Vamos organizar seus arquivos em pastas!")
+txt_main.grid(column=0, row=0, padx=10, pady=10)
+
+botao = Button(janela, text="Procurar pasta", command=orgArq)
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+txt_end = Label(janela, text="Seus arquivos agora estão organizados!")
+txt_end.grid_forget()
+
+ 
+janela.mainloop()
