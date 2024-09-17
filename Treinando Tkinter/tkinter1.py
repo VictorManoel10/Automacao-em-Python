5fc4dfc299bcb6c1b0e46d413c1b0682abd4b06a
Victor Manoel <victolopes2002@gmail.com>
2024-09-17 09:29:01 -0300

    Add pasta do meu progresso no tkinter

---
+++ Treinando Tkinter/tkinter1.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+janela = Tk() #Criar um janela
+janela.title("Olá Mundo!") #Titulo da janela
+janela.geometry("1000x500") #Configurar largura e comprimento da janela
+
+janela.mainloop() #Loop para manter a janela aberta
